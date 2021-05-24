ae2c795fac1fda0c71b2c8e93992e8321e6de5a4
harry-fruit <isaqueduarte17@gmail.com>
2021-05-24 19:29:09 -0300

    att

--- client/src/app/components/bodyComponents/about.jsx
+++ client/src/app/components/bodyComponents/about.jsx
@@ -7,8 +7,8 @@ export class About extends Component{
         return(
             <section className="bodyAbout">
 
-                <section className="firstRow">
                     <h1>About Us</h1>
+                <section className="firstRow">
                     <article>
                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium similique mollitia odit quasi unde quia possimus exercitationem, distinctio praesentium totam perspiciatis voluptatibus nihil? Veniam autem odio nostrum hic impedit enim!</p>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat nostrum optio ad laborum facilis temporibus, atque laudantium, adipisci consequuntur saepe libero fuga quas nesciunt minus amet eveniet! Eius, laudantium sequi?</p>
@@ -17,7 +17,7 @@ export class About extends Component{
 
                 <section className="secondRow">
                     <h2>Locales</h2>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d467692.048744584!2d-46.875489036252546!3d-23.681531483830113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce448183a461d1%3A0x9ba94b08ff335bae!2zU8OjbyBQYXVsbywgU1A!5e0!3m2!1spt-BR!2sbr!4v1620604981680!5m2!1spt-BR!2sbr"style={{border:0}} allowFullScreen="" loading="lazy"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d467692.048744584!2d-46.875489036252546!3d-23.681531483830113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce448183a461d1%3A0x9ba94b08ff335bae!2zU8OjbyBQYXVsbywgU1A!5e0!3m2!1spt-BR!2sbr!4v1620604981680!5m2!1spt-BR!2sbr" allowFullScreen="" loading="lazy"></iframe>
                     <p>*This map is purely ilustrative*</p>
                 </section>
             </section>
--- client/src/app/components/bodyComponents/modalities.jsx
+++ client/src/app/components/bodyComponents/modalities.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from "react"
 
+import bodybuilding from "../../../img/modalitiesPic/bodybuilding.jpg"
+import boxing from "../../../img/modalitiesPic/boxing.jpg"
+import crossfit from "../../../img/modalitiesPic/crossfit.jpg"
+import nutrition from "../../../img/modalitiesPic/nutrition.jpg"
+
 export class Modalities extends Component{
 
     render(){
@@ -8,6 +13,44 @@ export class Modalities extends Component{
             <section className="bodyModalities">
                 <section className="firstRow">
                     <h1>Modalities</h1>
+
+                    <section className="modalities">
+
+                        <figure className="modality">
+                            <img src={bodybuilding} alt="bodybuilding" />
+                            <div className="info">
+                                <figcaption>Bodybuilding</figcaption>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi recusandae consectetur cupiditate tempore fugit aut quidem quia ipsam non commodi, adipisci, veritatis explicabo reprehenderit, repellendus asperiores exercitationem fugiat! Quos, corrupti.</p>
+
+                            </div>
+                        </figure>
+
+                        <figure className="modality">
+                            <img src={boxing} alt="boxing" />
+                            <div className="info">
+                                <figcaption>Boxing</figcaption>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate et magni nostrum aspernatur voluptatibus nesciunt similique facere, velit iure expedita voluptate, eaque earum iste veritatis maxime id eligendi cumque. Nam.</p>
+                            </div>
+                        </figure>
+
+                        <figure className="modality">
+                            <img src={crossfit} alt="crossfit" />
+                            <div className="info">
+                                <figcaption>Crossfit</figcaption>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil sequi consectetur cum recusandae magnam repellendus similique sint asperiores iusto incidunt delectus provident adipisci dolorem laudantium iste, quos quasi error odit.</p>
+                            </div>
+                        </figure>
+
+                        <figure className="modality">
+                            <img src={nutrition} alt="crossfit" />
+                            <div className="info">
+                                <figcaption>Nutritional Education</figcaption>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil sequi consectetur cum recusandae magnam repellendus similique sint asperiores iusto incidunt delectus provident adipisci dolorem laudantium iste, quos quasi error odit.</p>
+                            </div>
+                        </figure>
+
+                    </section>
+
                 </section>
             </section>
         )
